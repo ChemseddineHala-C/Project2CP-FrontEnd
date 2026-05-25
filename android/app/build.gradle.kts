5e1aee1f8f0bdf969c11a28de5df96dec7583e7d
laptop <chemseddine.hala@gmail.com>
2026-05-25 19:33:39 +0100

    ...

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,9 @@
+import java.util.Properties
+
+val secretsFile = rootProject.file("secrets.properties")
+val secrets = Properties()
+secrets.load(secretsFile.inputStream())
+
 plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
@@ -19,6 +25,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["mapsApiKey"] = secrets.getProperty("GOOGLE_MAPS_API_KEY") ?: error("GOOGLE_MAPS_API_KEY not found in secrets.properties")
     }
 
     buildTypes {
